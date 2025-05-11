1b9c9d135405859ee24c5ef2009b7789fd5f57b8
moongmoongch <cuti0223@gmail.com>
2025-05-12 03:02:32 +0900

    feat: 더미 데이터 추가 #38

--- app/src/main/java/com/example/slo_plo/InstaShareFragment.kt
+++ app/src/main/java/com/example/slo_plo/InstaShareFragment.kt
@@ -33,6 +33,25 @@ class InstaShareFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        // ✅ 더미 텍스트 설정 후에 일지 상세보기에서 번들로 넘겨받도록 수정
+        binding.tvInstaShareName.text = "2025-05-11 플로깅 인증 (^ . ^)"
+        binding.tvInstaShareDistance.text = "3.8 km 플로깅 성공 ~ !"
+
+        binding.btnInstaShare.setOnClickListener {
+            // 흰색 배경을 비트맵으로 생성
+            val backgroundBitmap = drawBackgroundBitmap()
+
+            // 스티커 레이어 캡처
+            val stickerBitmap = captureViewAsBitmap(binding.imgStickerLayout)
+
+            // 배경과 스티커 각각 캐시로 저장
+            val backgroundUri = saveBitmapToCache(backgroundBitmap, "background_image.png")
+            val stickerUri = saveBitmapToCache(stickerBitmap, "sticker_image.png")
+
+            // Instagram 스토리로 배경과 스티커 이미지 각각 공유
+            shareToInstagramStory(backgroundUri, stickerUri)
+        }
+
         binding.btnInstaShare.setOnClickListener {
             // 흰색 배경을 비트맵으로 생성
             val backgroundBitmap = drawBackgroundBitmap()
